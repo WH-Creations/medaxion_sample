0aba1576ca4a92e159b1c25f5be0bded5bebdba3
Casey West <casey@westhousecreations.com>
2023-11-09 22:24:35 -0800

    Stub tests with mock service
    - Create protocol to be used for authentication services and setup test with yet to be created MockAuthenticationService

--- medaxion_sample/Services/Authentication/AuthenticationServiceProtocol.swift
+++ medaxion_sample/Services/Authentication/AuthenticationServiceProtocol.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+protocol AuthenticationServiceProtocol {
+    
+    /// Attempts to log in with a given username and password.
+    ///
+    /// - Parameters:
+    ///   - username: The username of the user attempting to log in.
+    ///   - password: The password of the user attempting to log in.
+    ///   - completion: A closure that is called with a Boolean value indicating whether the login was successful.
+    func login(username: String, password: String, completion: @escaping (Bool) -> Void)
+}

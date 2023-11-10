c877577af2a42394844a0b5366ec2fa2aa8820bb
Casey West <casey@westhousecreations.com>
2023-11-09 22:03:35 -0800

    Initial commit
    - Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# medaxion_sample
---
+++ medaxion_sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  medaxion_sample
+//
+//  Created by Casey West on 11/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

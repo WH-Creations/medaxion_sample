ba148408211019135dd4dd51c35a83643e2fb0bd
Casey West <casey@westhousecreations.com>
2023-11-09 22:17:53 -0800

    Add services files
    - Add service files and tests (both to be implemented)

---
+++ medaxion_sample/Services/Authentication/MockAuthenticationService.swift
@@ -0,0 +1,8 @@
+//
+//  MockAuthenticationService.swift
+//  medaxion_sample
+//
+//  Created by Casey West on 11/9/23.
+//
+
+import Foundation
---
+++ medaxion_sample/Services/Authentication/ProductionAuthenticationService.swift
@@ -0,0 +1,8 @@
+//
+//  ProductionAuthenticationService.swift
+//  medaxion_sample
+//
+//  Created by Casey West on 11/9/23.
+//
+
+import Foundation
